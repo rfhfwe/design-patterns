f76b3cd5085f33e462d82670f8f7ea5f4aa7194e
swy-yue <65485028+swy-yue@users.noreply.github.com>
2023-10-29 11:22:14 +0800

    生成器模式

---
+++ builder/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+var (
+	normalBuilder IBuilder
+	iglooBuilder  IBuilder
+
+	director *Director
+)
+
+func Init() {
+	normalBuilder = getBuilder("normal")
+	iglooBuilder = getBuilder("igloo")
+
+	director = newDirector(normalBuilder)
+}
+
+func main() {
+
+	normalHouse := director.buildHouse()
+	fmt.Printf("Normal House Door Type: %s\n", normalHouse.doorType)
+	fmt.Printf("Normal House Window Type: %s\n", normalHouse.windowType)
+	fmt.Printf("Normal House Num Floor: %d\n", normalHouse.floor)
+
+	director.setBuilder(iglooBuilder)
+	iglooHouse := director.buildHouse()
+	fmt.Printf("\nIgloo House Door Type: %s\n", iglooHouse.doorType)
+	fmt.Printf("Igloo House Window Type: %s\n", iglooHouse.windowType)
+	fmt.Printf("Igloo House Num Floor: %d\n", iglooHouse.floor)
+}
